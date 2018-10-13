0a62a03a1ea48ac54fe896cb744ad7d9d4d19e9b
Pramesti Hatta K <opam.qweiop@gmail.com>
2018-10-13 18:40:01 +0700

    add model

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/VanaraID/to-do-list/model"
 )
 
 func main() {
@@ -18,11 +21,22 @@ func main() {
 }
 
 func handlerIndex(w http.ResponseWriter, r *http.Request) {
-	message := "welcome to vanara"
-	w.Write([]byte(message))
+	OutputJSON(w, model.GetTodos())
 }
 
 func handlerTodo(w http.ResponseWriter, r *http.Request) {
 	message := "todos!"
 	w.Write([]byte(message))
+}
+
+func OutputJSON(w http.ResponseWriter, o interface{}) {
+	res, err := json.Marshal(o)
+	if err != nil {
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(res)
+	w.Write([]byte("\n"))
 }
---
+++ model/model.go
@@ -0,0 +1,29 @@
+package model
+
+var todos = []*Todo{}
+
+type Todo struct {
+	Id     int32
+	Name   string
+	Status bool
+}
+
+func init() {
+	todos = append(todos, &Todo{Id: 1, Name: "Makan", Status: true})
+	todos = append(todos, &Todo{Id: 2, Name: "Ngoding", Status: true})
+	todos = append(todos, &Todo{Id: 3, Name: "Tidur", Status: false})
+}
+
+func GetTodos() []*Todo {
+	return todos
+}
+
+func SelectTodo(id int32) *Todo {
+	for _, each := range todos {
+		if each.Id == id {
+			return each
+		}
+	}
+
+	return nil
+}
