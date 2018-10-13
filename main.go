65abf68106c4c19a1fa4fe751926fa0ac261e296
Pramesti Hatta K <opam.qweiop@gmail.com>
2018-10-14 02:30:23 +0700

    refactor

--- main.go
+++ main.go
@@ -1,42 +1,22 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-	"strconv"
-
-	"github.com/VanaraID/to-do-list/lib"
-	"github.com/VanaraID/to-do-list/model"
+	"github.com/VanaraID/to-do-list/response"
+	"github.com/go-chi/chi"
 )
 
+// NetworthAPI nw api struct
+type ServerVanara struct {
+	router   *chi.Mux
+	response *response.ResponseFormat
+}
+
 func main() {
-	http.HandleFunc("/", handlerIndex)
-	http.HandleFunc("/todo", handlerTodo)
 
-	var address = "localhost:6969"
-	fmt.Printf("server started at %s\n", address)
-	err := http.ListenAndServe(address, nil)
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-}
-
-func handlerIndex(w http.ResponseWriter, r *http.Request) {
-	lib.HTTPJSON(w, model.GetTodos())
-}
-
-func handlerTodo(w http.ResponseWriter, r *http.Request) {
-	if id := r.URL.Query().Get("id"); id != "" {
-
-		idInt, err := strconv.Atoi(id)
-
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
-
-		lib.HTTPJSON(w, model.SelectTodo(idInt))
-		return
+	server := &ServerVanara{
+		router:   chi.NewRouter(),
+		response: response.NewResponse(),
 	}
 
-	lib.HTTPError(w, http.StatusInternalServerError, "parameter id is a must.")
+	server.Start("localhost:6969")
 }
